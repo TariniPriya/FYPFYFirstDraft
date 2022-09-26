2c8d2e4024ad2a8a94d6496a33e37820b9c53190
TariniPriya <tarini7400@gmail.com>
2022-09-26 12:24:01 +0800

    draft4

--- app/src/main/java/com/example/fypfirstdraft/Login.java
+++ app/src/main/java/com/example/fypfirstdraft/Login.java
@@ -12,5 +12,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         editTextName=(EditText)findViewById(R.id.editTextName);
         editTextName=(EditText)findViewById(R.id.editTextName);
+        editTextName=(EditText)findViewById(R.id.editTextName);
     }
 }

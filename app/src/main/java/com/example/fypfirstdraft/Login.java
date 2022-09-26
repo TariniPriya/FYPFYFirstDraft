b74c89bc385cb0166d8c373a5bbee9c08427534f
Janis <janis.fong@yahoo.com>
2022-09-26 12:57:01 +0800

    Commit login

--- app/src/main/java/com/example/fypfirstdraft/Login.java
+++ app/src/main/java/com/example/fypfirstdraft/Login.java
@@ -2,16 +2,33 @@ package com.example.fypfirstdraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+EditText FName_Input, LName_Input, Email_Input, Contact_Input;
+Button Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        editTextName=(EditText)findViewById(R.id.editTextName);
-        editTextName=(EditText)findViewById(R.id.editTextName);
-        editTextName=(EditText)findViewById(R.id.editTextName);
+        FName_Input=(EditText)findViewById(R.id.FName_Input);
+        LName_Input=(EditText)findViewById(R.id.FName_Input);
+        Email_Input=(EditText)findViewById(R.id.Email_Input);
+        Contact_Input=(EditText)findViewById(R.id.Contact_Input);
+        Register=(Button)findViewById(R.id.Register);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

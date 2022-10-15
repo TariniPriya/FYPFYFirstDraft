906fe0d70c2eab39b1b01a632123fa8f079c1120
Janis <janis.fong@yahoo.com>
2022-10-16 02:55:36 +0800

    Commit files

--- app/src/main/java/com/example/fypfirstdraft/Login.java
+++ app/src/main/java/com/example/fypfirstdraft/Login.java
@@ -7,28 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-EditText FName_Input, LName_Input, Email_Input, Contact_Input;
-Button Register;
+EditText Email_Input;
+Button Login;
+TextView loginQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        FName_Input=(EditText)findViewById(R.id.FName_Input);
-        LName_Input=(EditText)findViewById(R.id.FName_Input);
         Email_Input=(EditText)findViewById(R.id.Email_Input);
-        Contact_Input=(EditText)findViewById(R.id.Contact_Input);
-        Register=(Button)findViewById(R.id.Register);
+        Login=(Button)findViewById(R.id.Register);
+        loginQuestion=(TextView)findViewById(R.id.LoginPageQuestion);
 
-        Register.setOnClickListener(new View.OnClickListener() {
-
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, MainPage.class);
                 startActivity(intent);
             }
         });
+
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/fypfirstdraft/Register.java
+++ app/src/main/java/com/example/fypfirstdraft/Register.java
@@ -2,13 +2,46 @@ package com.example.fypfirstdraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    EditText FName_Input, LName_Input, Password, ConfirmPassword, Email_Input, Contact_Input;
+    TextView back;
+    Button Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        FName_Input=(EditText)findViewById(R.id.FName_Input);
+        LName_Input=(EditText)findViewById(R.id.FName_Input);
+        Password=(EditText)findViewById(R.id.pw);
+        ConfirmPassword=(EditText)findViewById(R.id.cpw);
+        Email_Input=(EditText)findViewById(R.id.Email_Input);
+        Contact_Input=(EditText)findViewById(R.id.Contact_Input);
+        Register=(Button)findViewById(R.id.Register);
+        back= (TextView) findViewById(R.id.back);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, MainPage.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

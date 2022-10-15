b72e80208a1ffbaff434632b4dd5e47688c2fcab
Janis <janis.fong@yahoo.com>
2022-10-16 03:34:12 +0800

    Commit files

--- app/src/main/java/com/example/fypfirstdraft/Login.java
+++ app/src/main/java/com/example/fypfirstdraft/Login.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-EditText Email_Input;
+EditText Email_Input, Password;
 Button Login;
 TextView loginQuestion;
+boolean passwordVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Email_Input=(EditText)findViewById(R.id.Email_Input);
+        Password=(EditText)findViewById(R.id.pw);
         Login=(Button)findViewById(R.id.Register);
         loginQuestion=(TextView)findViewById(R.id.LoginPageQuestion);
 
@@ -38,6 +43,29 @@ TextView loginQuestion;
             }
         });
 
+        Password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int Right=2;
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    if(event.getRawX()>=Password.getRight()-Password.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=Password.getSelectionEnd();
+                        if(passwordVisible){
+                            Password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility_off, 0);
+                            Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else{
+                            Password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility, 0);
+                            Password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+                        }
+                        Password.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/fypfirstdraft/Register.java
+++ app/src/main/java/com/example/fypfirstdraft/Register.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ public class Register extends AppCompatActivity {
     EditText FName_Input, LName_Input, Password, ConfirmPassword, Email_Input, Contact_Input;
     TextView back;
     Button Register;
+    boolean passwordVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,54 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Register.this, Login.class);
                 startActivity(intent);
+            }
+        });
+
+        Password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int Right=2;
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    if(event.getRawX()>=Password.getRight()-Password.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=Password.getSelectionEnd();
+                        if(passwordVisible){
+                            Password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility_off, 0);
+                            Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else{
+                            Password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility, 0);
+                            Password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+                        }
+                        Password.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        ConfirmPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int Right=2;
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    if(event.getRawX()>=ConfirmPassword.getRight()-ConfirmPassword.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=ConfirmPassword.getSelectionEnd();
+                        if(passwordVisible){
+                            ConfirmPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility_off, 0);
+                            ConfirmPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else{
+                            ConfirmPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.visibility, 0);
+                            ConfirmPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+                        }
+                        ConfirmPassword.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
             }
         });
 

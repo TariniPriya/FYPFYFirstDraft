46be6280fae88459ac5c993591cb3e81391b9ccf
TariniPriya <tarini7400@gmail.com>
2022-11-03 18:56:03 +0800

    updatess

--- app/src/main/java/com/example/fypfirstdraft/Register.java
+++ app/src/main/java/com/example/fypfirstdraft/Register.java
@@ -2,6 +2,7 @@ package com.example.fypfirstdraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
@@ -9,13 +10,17 @@ import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class Register extends AppCompatActivity {
-    EditText FName_Input, DOB, Password, ConfirmPassword, Email_Input, Contact_Input;
+    EditText FName_Input, DOB, Gender, Password, ConfirmPassword, Email_Input, Contact_Input;
     TextView back;
     Button Register;
+    DatePickerDialog picker;
     boolean passwordVisible;
 
     @Override
@@ -24,6 +29,7 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         FName_Input=(EditText)findViewById(R.id.FName_Input);
         DOB=(EditText)findViewById(R.id.DOB_Input);
+        Gender=(EditText)findViewById(R.id.Gender_Input);
         Password=(EditText)findViewById(R.id.pw);
         ConfirmPassword=(EditText)findViewById(R.id.cpw);
         Email_Input=(EditText)findViewById(R.id.Email_Input);
@@ -38,6 +44,26 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //Setting up DatePicker on EditText
+        DOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                //Date Picker Dialog
+                picker = new DatePickerDialog(Register.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        DOB.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                },year, month, day);
+                picker.show();
+            }
+        });
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,10 +125,9 @@ public class Register extends AppCompatActivity {
 }
 
 /*
-decided to include Date of Birth. So need change the format to calendar
 forgot we need to include gender as well. for this, we can input radio-button (male, female, others)
 ensure user keys in the deetz before proceeding to the next activity. Same for Login activity
 age
-Firebase
+Firebase/azuresql
 not sure if yall want do the OTP thing. tat one need firebase also
  */
